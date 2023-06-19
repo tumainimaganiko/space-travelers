e8f0f651420b465d4b3e705e1ad0f24515a51c3b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-19 17:39:57 +0300

    Update RocketDetails Styles

--- src/components/RocketDetails.jsx
+++ src/components/RocketDetails.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import styles from "../styles/RocketDetails.module.css"
-import img from "../Images/space-img.png";
+import styles from "../styles/RocketDetails.module.css";
 
 const RocketDetails = () => {
   return (
-    <div>
-      <img src={img} className={styles.img} alt="" />
-      <div className="details">
+    <div className={styles.container}>
+      <img
+        src="https://t3.ftcdn.net/jpg/05/58/59/78/360_F_558597869_9iWHODEeht6tqGHU8O0MsJOVTl2RamFu.jpg"
+        className={styles.img}
+        alt=""
+      />
+      <div className={styles.details}>
         <h2>Falcon 1</h2>
-        <p className="description">
+        <p>
           The Falcon 1 was an expendable launch system privately developed and
           manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon
           1 became the first privately-developed liquid-fuel launch vehicle to
