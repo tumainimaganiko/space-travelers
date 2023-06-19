3bb857762cd9be84fed4e5a9a7ac525e0877caee
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-19 15:20:45 +0300

    Use Routes for components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="myprofile" element={<MyProfile />} />
+      </Route>
+    </Routes>
   );
 }
 
