4f63acc7e18846bb08728b488625a42b6b69a4d3
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-20 13:49:11 +0300

    Pass Reserved Prop

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -16,9 +16,11 @@ const Rockets = () => {
       {rockets.map((item) => (
         <RocketDetails
           key={item.id}
+          id={item.id}
           name={item.name}
           description={item.description}
           image={item.flickr_images}
+          reserved={item.reserved}
         />
       ))}
     </div>
