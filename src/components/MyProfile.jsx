992232aa48fe06bd924dcdc8b801385ad61ff63d
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-20 21:25:01 +0300

    Render List of Booked Rocket

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,5 +1,15 @@
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+import { useSelector } from 'react-redux';
+
+const MyProfile = () => {
+  const { rockets } = useSelector((store) => store.rockets);
+  const bookedRockets = rockets.filter((rocket) => rocket.reserved);
+  return (
+    <ul>
+      {bookedRockets.map((rocket) => (
+        <li key={rocket.id}>{rocket.name}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default MyProfile;
