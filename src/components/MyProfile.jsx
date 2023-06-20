efb2025e8e024789d77100650f8049b1f48fc704
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-20 23:16:12 +0300

    Update MyProfile Component

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,15 +1,18 @@
 import { useSelector } from 'react-redux';
+import styles from '../styles/MyProfile.module.css';
 
 const MyProfile = () => {
   const { rockets } = useSelector((store) => store.rockets);
   const bookedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
-    <table>
-      <th>Rockets</th>
-      {bookedRockets.map((rocket) => (
-        <tr key={rocket.id}>{rocket.name}</tr>
-      ))}
-    </table>
+    <div className={styles.container}>
+      <h2>My Rockets</h2>
+      <ul>
+        {bookedRockets.map((rocket) => (
+          <li key={rocket.id}>{rocket.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
