d3d34a0df5f8e80bc27d0f51b6512e3c5a7931f4
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 20:07:01 +0300

    Add Rockets Test

---
+++ src/test/Rockets.test.js
@@ -0,0 +1,29 @@
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import myRockets from './rocketData';
+import Rockets from '../components/Rockets';
+
+jest.mock('axios');
+
+const reducer = (state = {
+  rockets: { rocket: myRockets },
+}) => state;
+
+const store = configureStore({ reducer });
+
+describe('Testing the Rocket Component', () => {
+  it('Test the rockets data', () => {
+    axios.get.mockResolvedValue({ data: myRockets });
+    const tree = renderer.create(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Rockets />
+        </Provider>
+      </MemoryRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
