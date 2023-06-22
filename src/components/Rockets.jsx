2a873c7ac3f96b8ca31b49c4e13572daaf691a90
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 08:35:52 +0300

    Add Unique keys

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,21 +1,32 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import RocketDetails from './RocketDetails';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 const Rockets = () => {
   const { rockets } = useSelector((store) => store.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
 
   return (
     <div>
-      {rockets.map((item) => (
-        <RocketDetails
-          key={item.id}
-          id={item.id}
-          name={item.name}
-          description={item.description}
-          image={item.flickr_images}
-          reserved={item.reserved}
-        />
-      ))}
+      {
+        rockets.length > 0 ? (
+          rockets.map((item) => (
+            <RocketDetails
+              key={uuidv4()}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              image={item.flickr_images}
+              reserved={item.reserved}
+            />
+          ))
+        ) : 'loading'
+      }
     </div>
   );
 };
