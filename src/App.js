6d3f32ed7e774d04c0063d69870099d82a34ad9f
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 08:36:50 +0300

    Remove Dispatching Actions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
-import { fetchMissions } from './redux/missions/missionsSlice';
-import { fetchRockets } from './redux/rockets/rocketsSlice';
 
 const Layout = () => (
   <>
@@ -16,12 +12,6 @@ const Layout = () => (
   </>
 );
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchMissions());
-    dispatch(fetchRockets());
-  }, [dispatch]);
-
   return (
     <Routes>
       <Route
