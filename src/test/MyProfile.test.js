e0ce4e0f9fa5aeaf7ca5435e4e711661121520a5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 21:17:44 +0300

    Test MyProfile Component

---
+++ src/test/MyProfile.test.js
@@ -0,0 +1,32 @@
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import myRockets from './rocketData';
+import MyProfile from '../components/MyProfile';
+import myMissions from './missionsData';
+
+jest.mock('axios');
+
+const reducer = (state = {
+  missions: { missions: myMissions },
+  rockets: { rockets: myRockets },
+}) => state;
+
+const store = configureStore({ reducer });
+
+describe('Test MyProfile component', () => {
+  it('shouls show reserved Rockets and Missions', () => {
+    axios.get.mockResolvedValue({ data: myMissions });
+    axios.get.mockResolvedValue({ data: myRockets });
+    const tree = renderer.create(
+      <MemoryRouter>
+        <Provider store={store}>
+          <MyProfile />
+        </Provider>
+      </MemoryRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
