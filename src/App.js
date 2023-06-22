2b60bdcfa80871b5c4ce0f76b9eb338eb3509775
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 13:01:23 +0300

    Dispatch fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchRockets } from './redux/rockets/rocketsSlice';
 import './App.css';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
+import { fetchMissions } from './redux/missions/missionsSlice';
 
 const Layout = () => (
   <>
@@ -12,6 +16,11 @@ const Layout = () => (
   </>
 );
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+    dispatch(fetchMissions());
+  });
   return (
     <Routes>
       <Route
