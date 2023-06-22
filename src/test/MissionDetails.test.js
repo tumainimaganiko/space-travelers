28e2acfac8a3b27b5f77a6038f92ea4c09a5d835
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 21:05:20 +0300

    Test MissionDetails Components

---
+++ src/test/MissionDetails.test.js
@@ -0,0 +1,22 @@
+import renderer from 'react-test-renderer';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import MissionDetails from '../components/MissionDetails';
+import myMissions from './missionsData';
+
+
+const reducer = (state = {
+    missions: { missions: myMissions },
+  }) => state;
+
+  const store = configureStore({ reducer });
+
+it('Should render Mission details', () => {
+    const tree = renderer.create(
+        <Provider store={store}>
+        <MissionDetails name={'space 1'} description={'first space'} id={'firstItem'} reserved />
+        </Provider>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+})
