a7199041ba789ab5370731cf2c4bf0d07bfc2b88
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-22 08:34:27 +0300

    Add Unique keys

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { fetchMissions } from '../redux/missions/missionsSlice';
 import MissionDetails from './MissionDetails';
 import Styles from '../styles/Mission.module.css';
 
 const Missions = () => {
   const { missions } = useSelector((store) => store.missions);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
   return (
     <table className={Styles.missionTable}>
       <thead>
@@ -13,15 +22,17 @@ const Missions = () => {
           <th>Status</th>
         </tr>
       </thead>
-      {missions.map((mission) => (
-        <MissionDetails
-          key={mission.mission_id}
-          name={mission.mission_name}
-          description={mission.description}
-          id={mission.mission_id}
-          reserved={mission.reserved}
-        />
-      ))}
+      <tbody>
+        {missions.map((mission) => (
+          <MissionDetails
+            key={uuidv4()}
+            name={mission.mission_name}
+            description={mission.description}
+            id={mission.mission_id}
+            reserved={mission.reserved}
+          />
+        ))}
+      </tbody>
     </table>
   );
 };
