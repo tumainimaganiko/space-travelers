c18275159362bc859c2f84ff81fbeb4c0d9acee4
Amanuel Belete <amanuelbelete95@gmail.com>
2023-06-22 17:12:28 +0300

    Style the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,53 @@ const Navbar = () => {
     width: '40px',
     height: '40px',
   };
+
+  const activeLink = {
+    textDecoration: 'underline',
+    color: 'blue',
+    fontWeight: '500',
+  }
+
+  const link = {
+    textDecoration: 'none',
+    color: 'blue',
+    fontWeight: '500',
+  }
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
-        <img src={Img} style={imgStyle} alt="space logo" />
+        <img
+          src={Img}
+          style={imgStyle}
+          alt="space logo"
+        />
         <h1>Space Travelers&apos; Hub</h1>
       </div>
       <ul className={styles.list}>
-        <li><NavLink to="/">Rockets</NavLink></li>
-        <li><NavLink to="missions">Missions</NavLink></li>
-        <li><NavLink to="myprofile">My profile</NavLink></li>
+        <li>
+          <NavLink
+            to="/" 
+            style={({isActive}) => isActive ? activeLink : link}
+          >
+            Rockets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="missions"
+            style={({isActive}) => isActive ? activeLink : link}
+          >
+            Missions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="myprofile"
+            style={({isActive}) => isActive ? activeLink : link}
+          >
+            My profile
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
